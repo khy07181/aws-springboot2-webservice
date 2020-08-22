4edc2f4a392d38bfc8b9949dd12f74e65a863e92
Kimhayoung <khy07181@gmail.com>
2020-08-22 17:33:56 +0900

    게시글 수정, 삭제 화면 만들기

--- src/main/java/ex/example/springboot/web/PostsApiController.java
+++ src/main/java/ex/example/springboot/web/PostsApiController.java
@@ -17,9 +17,16 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
         return postsService.save(requestDto);
     }
+
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
         return postsService.update(id, requestDto);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
     }
 
     @GetMapping("/api/v1/posts/{id}")
