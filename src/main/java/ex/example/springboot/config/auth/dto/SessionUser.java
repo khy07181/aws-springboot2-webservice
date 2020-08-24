f01218f455ecec80f073ee3804d31b7b521da44b
Kimhayoung <khy07181@gmail.com>
2020-08-24 15:16:32 +0900

    네이버 로그인 연동

--- src/main/java/ex/example/springboot/config/auth/dto/SessionUser.java
+++ src/main/java/ex/example/springboot/config/auth/dto/SessionUser.java
@@ -3,8 +3,10 @@ package ex.example.springboot.config.auth.dto;
 import ex.example.springboot.domain.user.User;
 import lombok.Getter;
 
+import java.io.Serializable;
+
 @Getter
-public class SessionUser {
+public class SessionUser implements Serializable {
     private String name;
     private String email;
     private String picture;
