d429fadc26991ade1d6634de3e48cc92cf89ec5b
Kimhayoung <khy07181@gmail.com>
2020-08-24 15:48:34 +0900

    기존 테스트에 시큐리티 적용

---
+++ src/main/java/ex/example/springboot/config/JpaConfig.java
@@ -0,0 +1,9 @@
+package ex.example.springboot.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaConfig {
+}
